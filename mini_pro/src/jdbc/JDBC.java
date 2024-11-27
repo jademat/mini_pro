cad3c90c1b5be9a8add398fa9fa900bd8a21824b
jademat <120172426+jademat@users.noreply.github.com>
2024-11-27 10:14:20 +0900

    jdbc 추가 배경이미지 추가 틀 생성

--- mini_pro/src/jdbc/JDBC.java
+++ mini_pro/src/jdbc/JDBC.java
@@ -1,5 +1,57 @@
 package jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class JDBC {
+	
+	public Connection con = null;
+	public PreparedStatement pstmt = null;
+	public ResultSet res = null;
+	public String sql = null;
+	
+	
+	public void connect() {
+	
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "mini_pro";
+		String password = "1234";
+		
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url,user,password);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void close(Connection con, PreparedStatement pstmt, ResultSet res) {
+			try {
+				if(res != null) res.close();
+				if(pstmt != null)pstmt.close();
+				if(con != null)con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}		
+	}
+	
+	public void close(Connection con, PreparedStatement pstmt) {
+			try {
+				if(pstmt != null)pstmt.close();
+				if(con != null)con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+
 
 }

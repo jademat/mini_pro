cf661c749b60aa874ed442d70d0bbe054998855e
leegwangje <102304449+leegwangje@users.noreply.github.com>
2024-11-28 12:12:21 +0900

    Update je.sql

--- je.sql
+++ je.sql
@@ -1,1 +1,6 @@
--- je table
+create table WORKOUT (
+    ex_no number primary key,
+    ex_name varchar2(300) not null,
+    ex_txt varchar2(2000) not null,
+    ex_category varchar2(50) not null);
+    ex_img varchar2(1000) no null);
